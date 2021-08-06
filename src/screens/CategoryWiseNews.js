bf94c4913c5a9f4df1b05e8bd10d0e746067ccd4
kothariji <kotharidhruv25@gmail.com>
2021-08-06 23:48:59 +0530

    removed additional imports

--- src/screens/CategoryWiseNews.js
+++ src/screens/CategoryWiseNews.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import NewsArticleListItem from '../components/NewsArticleListItem';
 
 const CategoryWiseNews = ({ route }) => {
