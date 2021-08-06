bfe7472de459fe3c621fd6410ca7a002d60d18f9
kothariji <kotharidhruv25@gmail.com>
2021-08-07 02:00:23 +0530

    added top highlights for hoae

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import Loader from '../components/Loader';
+import NewsArticleListItem from '../components/NewsArticleListItem';
 
 const HomeScreen = () => {
-  return (
+
+  const [newsArticleList, setNewsArticleList] = useState([]);
+  const [showLoadingScreen, setShowLoadingScreen] = useState(false);
+
+  const fetchTopHighlights = () => {
+
+    setShowLoadingScreen(true);
+
+    fetch(`https://toofan-xpress-backend.herokuapp.com/top-headlines`)
+      .then(response => (response.json()))
+      .then(data => {
+        setShowLoadingScreen(false);
+        setNewsArticleList(data.articles);
+      })
+      .catch(error => console.error(error));
+  };
+
+
+  useEffect(() => {
+    fetchTopHighlights();
+  }, []);
+
+  let childToRender = showLoadingScreen ? (<Loader />) : (
     <View style={styles.homeScreen}>
-      <Text>Latest News Will come Here</Text>
-      <Text>Latest News Will come Here</Text>
-      <Text>Latest News Will come Here</Text>
-      <Text>Latest News Will come Here</Text>
-      <Text>Latest News Will come Here</Text>
-    </View>
-  );
+      <ScrollView style={styles.newsList}>
+        {newsArticleList.map((article, index) => (
+          <NewsArticleListItem article={article} key={index} />
+        ))}
+      </ScrollView>
+    </View>);
+
+  return childToRender;
 };
+
+
+
+
 
 const styles = StyleSheet.create({
   homeScreen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  newsList: {
+    marginTop: 10,
   },
 });
 
